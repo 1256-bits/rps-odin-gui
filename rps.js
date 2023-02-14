568629d2ad1825dc7bb80b71280f95dfa5da255e
128bit <128bit@eclipso.eu>
2023-02-14 23:08:18 +0300

    add typewriter effect to intro

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
 const rps = (player, ai) => {
-	const dict = {"R": "rock", "P": "paper", "S": "scissors"};
+	const dict = { "R": "rock", "P": "paper", "S": "scissors" };
 	const pva = `${dict[player]} vs ${dict[ai]}.`;
 	if (player === ai)
 		return pva + "Draw";
@@ -25,18 +25,24 @@ const rps = (player, ai) => {
 const plInput = () => prompt("[R]ock, [P]aper, [S]cissors");
 
 const aiInput = () => {
-	const rps = {0: "R", 1: "P", 2: "S" };
-	return rps[Math.trunc(Math.random()*3)];
+	const rps = { 0: "R", 1: "P", 2: "S" };
+	return rps[Math.trunc(Math.random() * 3)];
 }
 
 function rollIntro() {
+	
 	const intro = document.querySelector("#intro");
 	const lines = ["A.D. 2101", "War was beginning", "To keep all your base belong to you",
-					"You must win the game of rock, paper, scissors", "For great justice!"];
+		"You must win the game of rock, paper, scissors", "For great justice!"];
+	let delay = 50;
 	for (let line of lines) {
 		const p = document.createElement("p");
 		intro.appendChild(p);
-		p.innerText = line;
+		for (let char of line) {
+			console.log(char);
+			setTimeout(() => p.textContent += char, delay);
+			delay += 50;
+		}
 	}
 }
 
