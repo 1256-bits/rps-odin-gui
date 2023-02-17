b819003d7c237a7a3315ec6436c21a031ea2530c
128bit <128bit@eclipso.eu>
2023-02-17 14:39:14 +0300

    Make close button fade in, fix last child's animation

--- intro.js
+++ intro.js
@@ -1,24 +1,23 @@
-const delay = 50;
+const delay = 0;
 let i = 0;
 const intro = document.querySelector("#intro");
-
 
 function rollIntro(lineNum) {
     let isLastLine = false;
     const lines = ["A.D. 2101", "War was beginning", "To keep all your base belong to you",
         "You must win the game of rock, paper, scissors", "For great justice!"];
     const line = lines[lineNum];
-    
+
     if (!line) {
         setTimeout(drawCloseButton, 1000);
         return;
     }
-    
+
     const p = document.createElement("p");
     p.textContent = getRandomStr(line.length);
     p.style.display = "none";
     intro.appendChild(p);
-    
+
     if (lineNum == lines.length - 1)
         isLastLine = true;
     setTimeout(typewriter, 1000, p, line, lineNum, isLastLine);
@@ -40,29 +39,32 @@ function getRandomStr(len) {
 function typewriter(p, line, lineNum, isLastLine) {
     p.style.display = "block";
     p.textContent = line.slice(0, i + 1) + p.textContent.slice(i + 1);
-    
+
     if (i == 0 && isLastLine)
         p.style.width = getComputedStyle(p)["width"];
-    
+
     if (i == 0 && lineNum > 0) {
         const children = (isLastLine) ? intro.querySelectorAll("p") : intro.querySelectorAll(":not(:last-child)");
         const width = +getComputedStyle(intro)["width"].slice(0, -2) - 20 + "px";
         children.forEach(child => child.style.width = width);
     }
-    
+
     if (i >= line.length) {
         i = 0;
-        p.style.width = getComputedStyle(p)["width"];
+        if (!isLastLine)
+            p.style.width = getComputedStyle(p)["width"];
         rollIntro(lineNum + 1)
         return;
     }
 
     i++;
-    setTimeout(typewriter, delay, p, line, lineNum);
+    setTimeout(typewriter, delay, p, line, lineNum, isLastLine);
 }
 
 
 function drawCloseButton() {
+    const close = document.querySelector("#close-intro");
+    close.classList.replace("op-0", "op-100");
 }
 
 rollIntro(0);
