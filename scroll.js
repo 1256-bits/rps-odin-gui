28a63cfbc77a2736f28ff67c4457ce52eac1b71c
128bit <128bit@eclipso.eu>
2023-02-19 01:23:57 +0300

    make items centred

--- scroll.js
+++ scroll.js
@@ -2,7 +2,7 @@ window.addEventListener("keydown", (e) => {
     const root = document.querySelector(":root");
     const y = +getComputedStyle(root).getPropertyValue("--y").replace("px", "");
     if (e.key === "ArrowUp")
-        root.style.setProperty("--y", y - 2 + "px");
+        root.style.setProperty("--y", y - 21 + "px");
     else if (e.key === "ArrowDown")
-        root.style.setProperty("--y", y + 2 + "px");
+        root.style.setProperty("--y", y + 21 + "px");
 });
