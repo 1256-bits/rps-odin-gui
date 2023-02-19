b4a4ee174b71c488f51f1a8aab934b1170d99369
128bit <128bit@eclipso.eu>
2023-02-19 14:42:44 +0300

    Switch classes around when scrolling

--- scroll.js
+++ scroll.js
@@ -1,17 +1,22 @@
 window.addEventListener("keydown", (e) => {
     const root = document.querySelector(":root");
     const current = document.querySelector(".current");
-    const next = current.querySelector("::after");
-    const y = +getComputedStyle(root).getPropertyValue("--y").replace("px", "");
-    
+    const elements = Array.from(document.querySelector(".wrapper").children);
+    const after = elements[elements.indexOf(current) + 1];
+    const before = elements[elements.indexOf(current) - 1];
+
+    const y = +getComputedStyle(root).getPropertyValue("--y").replace("em", "");
+
     if (e.key === "ArrowUp") {
-        root.style.setProperty("--y", y - 21 + "px");
+        root.style.setProperty("--y", y - 1.5 + "em");
         current.classList.remove("current");
-        current.classList.add("op-0");
-        next.classList.remove("op-0");
-        next.classList.add("current");
+        // current.classList.add("op-0");
+        // after.classList.remove("op-0");
+        before.classList.add("current");
     }
     else if (e.key === "ArrowDown") {
-        root.style.setProperty("--y", y + 21 + "px");
+        root.style.setProperty("--y", y + 1.5 + "em");
+        current.classList.remove("current");
+        after.classList.add("current");
     }
 });
