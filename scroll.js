3f5626f3e466e137046f9ab73e6f5d5d003b8711
128bit <128bit@eclipso.eu>
2023-02-19 15:31:19 +0300

    fail

--- scroll.js
+++ scroll.js
@@ -1,7 +1,8 @@
 window.addEventListener("keydown", (e) => {
     const root = document.querySelector(":root");
     const current = document.querySelector(".current");
-    const elements = Array.from(document.querySelector(".wrapper").children);
+    const wrapper = document.querySelector(".wrapper");
+    const elements = Array.from(wrapper.children);
     const after = elements[elements.indexOf(current) + 1];
     const before = elements[elements.indexOf(current) - 1];
 
@@ -10,13 +11,17 @@ window.addEventListener("keydown", (e) => {
     if (e.key === "ArrowUp") {
         root.style.setProperty("--y", y - 1.5 + "em");
         current.classList.remove("current");
+        wrapper.append(current.cloneNode(true));
         // current.classList.add("op-0");
         // after.classList.remove("op-0");
-        before.classList.add("current");
+        wrapper.querySelector(":first-child").remove();
+        after.classList.add("current");
     }
     else if (e.key === "ArrowDown") {
         root.style.setProperty("--y", y + 1.5 + "em");
         current.classList.remove("current");
-        after.classList.add("current");
+        before.classList.add("current");
+        const newNode = current.cloneNode(true);
+        // wrapper.prepend();
     }
 });
