17b7f1a233b7bfdd165ca6db2565156f5ea44d71
128bit <128bit@eclipso.eu>
2023-02-19 19:51:34 +0300

    add somehow working endless scrolling in one direction

--- scroll.js
+++ scroll.js
@@ -1,10 +1,33 @@
 window.addEventListener("keydown", (e) => {
     const root = document.querySelector(":root");
     const current = document.querySelector(".current");
-    const ul = document.querySelector(".ul");
+    const wrapper = document.querySelector(".wrapper");
+    const previous = document.querySelector(".previous");
+    const next = document.querySelector(".next");
+    const hidden = document.querySelectorAll(".disable");
+    const begin = document.querySelector(".begin");
+    const end = document.querySelector(".end");
+    const y = +getComputedStyle(root).getPropertyValue("--y").replace("em", "");
+    const wrapperY = +getComputedStyle(root).getPropertyValue("--wrapper-y").replace("em", "");
+    console.log(wrapperY)
 
     if (e.key === "ArrowUp") {
     }
     else if (e.key === "ArrowDown") {
+        hidden[0].remove();
+        begin.classList.replace("begin", "disable");
+        previous.classList.replace("previous", "begin");
+        current.classList.replace("current", "previous");
+        next.classList.replace("next", "current");
+        end.classList.replace("end","next");
+        hidden[1].classList.replace("disable","end");
+        root.style.setProperty("--y", y - 1 + "em");
+        root.style.setProperty("--wrapper-y", wrapperY + 1 + "em");
+        const newHidden = next.cloneNode(true)
+        newHidden.classList.replace("current","disable");
+        wrapper.append(newHidden);
+
+        // hidden[1].classList.replace("disable", "next");
+        // previous.classList.replace("previous", "disable");
     }
 });
