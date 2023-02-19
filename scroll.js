6a213156b1ecc73caaedeba018ebe50a30feb442
128bit <128bit@eclipso.eu>
2023-02-19 13:38:56 +0300

    test another scroll idea

--- scroll.js
+++ scroll.js
@@ -1,8 +1,17 @@
 window.addEventListener("keydown", (e) => {
     const root = document.querySelector(":root");
+    const current = document.querySelector(".current");
+    const next = current.querySelector("::after");
     const y = +getComputedStyle(root).getPropertyValue("--y").replace("px", "");
-    if (e.key === "ArrowUp")
+    
+    if (e.key === "ArrowUp") {
         root.style.setProperty("--y", y - 21 + "px");
-    else if (e.key === "ArrowDown")
+        current.classList.remove("current");
+        current.classList.add("op-0");
+        next.classList.remove("op-0");
+        next.classList.add("current");
+    }
+    else if (e.key === "ArrowDown") {
         root.style.setProperty("--y", y + 21 + "px");
+    }
 });
