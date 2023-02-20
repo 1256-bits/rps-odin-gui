e58f3077afd242c309cd15c03d456d9219c158c5
128bit <128bit@eclipso.eu>
2023-02-20 15:35:02 +0300

    Add scrolling in opposite direction

--- scroll.js
+++ scroll.js
@@ -16,24 +16,38 @@ window.addEventListener("keydown", (e) => {
     const wrapperY = +getComputedStyle(root).getPropertyValue("--wrapper-y").replace("em", "");
 
     if (e.key === "ArrowUp") {
+        hidden[1].remove();
+        end.classList.replace("end", "disable");
+        previous.classList.replace("previous", "current");
+        next.classList.replace("next", "end");
+        current.classList.replace("current", "next");
+        begin.classList.replace("begin", "previous");
+        hidden[0].classList.replace("disable", "begin");
+        root.style.setProperty("--y", y + 1 + "em");
+        root.style.setProperty("--wrapper-y", wrapperY - 1 + "em");
+        const newHidden = previous.cloneNode(true)
+        newHidden.classList.replace("current", "disable");
+        wrapper.prepend(newHidden);
+
+        rateLimit = true;
+        setTimeout(() => rateLimit = false, 150);
     }
+
     else if (e.key === "ArrowDown") {
         hidden[0].remove();
         begin.classList.replace("begin", "disable");
         previous.classList.replace("previous", "begin");
         current.classList.replace("current", "previous");
         next.classList.replace("next", "current");
-        end.classList.replace("end","next");
-        hidden[1].classList.replace("disable","end");
+        end.classList.replace("end", "next");
+        hidden[1].classList.replace("disable", "end");
         root.style.setProperty("--y", y - 1 + "em");
         root.style.setProperty("--wrapper-y", wrapperY + 1 + "em");
         const newHidden = next.cloneNode(true)
-        newHidden.classList.replace("current","disable");
+        newHidden.classList.replace("current", "disable");
         wrapper.append(newHidden);
-        
+
         rateLimit = true;
         setTimeout(() => rateLimit = false, 150);
-        // hidden[1].classList.replace("disable", "next");
-        // previous.classList.replace("previous", "disable");
     }
 });
