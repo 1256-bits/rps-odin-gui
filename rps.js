68883f5e9e0efe90bc402dc76849590e74921a1e
128bit <128bit@eclipso.eu>
2023-03-03 20:21:05 +0300

    combine draw/loss/win in asingle function

--- rps.js
+++ rps.js
@@ -1,14 +1,14 @@
 function rps(player, ai) {
 	if (player === ai) {
-		draw();
+		result("draw");
 	}
 	else if ((player === "rock" && ai === "scissors") ||
 		(player === "paper" && ai === "rock") ||
 		(player === "scissors" && ai === "paper")) {
-		win();
+		result("win", "loss");
 	}
 	else {
-		loss();
+		result("loss", "win");
 	}
 }
 
@@ -22,14 +22,14 @@ function getAi() {
 	return document.querySelector("#ai-wrap > .current").textContent;
 }
 
-function win() {
-
-}
-
-function loss() {
-
-}
-
-function draw() {
-
+function result(result, aiResult = "draw") {
+	const bg = document.querySelector("#main-screen");
+	const [player, ai] = [...document.querySelectorAll(".rps-window")];
+	const colorSwap = () => {
+		player.classList.toggle(result);
+		ai.classList.toggle(aiResult);
+	}
+	colorSwap();
+	setTimeout(colorSwap, 10000);
+	
 }
