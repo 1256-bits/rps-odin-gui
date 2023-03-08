1a11a5c482bb9e8b84b067caa69e1bb1276c9a45
128bit <128bit@eclipso.eu>
2023-03-08 12:18:35 +0300

    Fix finish failing to start, change formatting

--- rps.js
+++ rps.js
@@ -58,21 +58,29 @@ function animToggle() {
 
 }
 
+const getNewWidth = (width, state, fontSize) => {
+	const vals = { win: 1.5, loss: -6 };
+	const retVal = width / fontSize + vals[state];
+	return `${(retVal > 0) ? retVal : 0}rem`;
+}
+
 function updateBars(player, ai) {
 	if (player === "draw")
 		return true;
 	let returnVal = true;
-	const vals = { win: 1.5, loss: -6 };
+	const bars = document.querySelectorAll(".bar");
 	const fontSize = +getComputedStyle(document.querySelector(":root"))
 		.getPropertyValue("font-size").replace("px", "");
-	const bars = document.querySelectorAll(".bar");
+
 	bars.forEach(bar => {
 		const width = +getComputedStyle(bar).getPropertyValue("width").replace("px", "");
 		bar.style.width = (bar.hasAttribute("data-player")) ?
-			`${width / fontSize + vals[player]}rem` :
-			`${width / fontSize + vals[ai]}rem`;
+			getNewWidth(width, player, fontSize) : getNewWidth(width, ai, fontSize);
+
 		if (+getComputedStyle(bar).getPropertyValue("width").replace("px", "") > 12 * fontSize)
 			bar.style.width = "12rem";
+		console.log(bar.hasAttribute("data-player"), bar.style.width)
+
 		if (+bar.style.width.replace("rem", "") <= 0) {
 			finish(!bar.hasAttribute("data-player"));
 			returnVal = false;
@@ -85,6 +93,7 @@ function finish(won) {
 	const wrapper = document.querySelector("div:has(.finish-card)");
 	const card = document.querySelector(".finish-card");
 	wrapper.classList.replace("hide", "flex");
+
 	setTimeout(() => {
 		card.classList.replace("op-0", "op-95");
 		card.classList.remove("move-in");
@@ -96,42 +105,44 @@ function finish(won) {
 function reset(e) {
 	const wrapper = document.querySelector("div:has(.finish-card)");
 	const card = document.querySelector(".finish-card");
+	const aiWrap = document.querySelector("#ai-wrap");
+	const bars = document.querySelectorAll(".bar");
+	const main = document.querySelector("#main-screen");
+	const rpsWindows = document.querySelectorAll(".rps-window");
+
+	localStorage.clear();
 	card.classList.replace("op-95", "op-0");
 	wrapper.style.backgroundColor = "rgb(100,100,100)";
-	localStorage.clear();
 	document.querySelector(".wrapper").innerHTML = `
-	<div class="disable">rock</div>
-	<div class="begin">paper</div>
-	<div class="previous">scissors</div>
-	<div class="current">rock</div>
-	<div class="next">paper</div>
-	<div class="end">scissors</div>
-	<div class="disable">rock</div>
+		<div class="disable">rock</div>
+		<div class="begin">paper</div>
+		<div class="previous">scissors</div>
+		<div class="current">rock</div>
+		<div class="next">paper</div>
+		<div class="end">scissors</div>
+		<div class="disable">rock</div>
 	`
-	const aiWrap = document.querySelector("#ai-wrap");
 	aiWrap.innerHTML = "";
 	addElements(30);
 	aiWrap.children[0].classList.add("previous");
 	aiWrap.children[1].classList.add("current");
 	aiWrap.children[2].classList.add("next");
-	const bars = document.querySelectorAll(".bar");
-	document.querySelectorAll(".rps-window").forEach(window => {
+	bars.forEach(bar => bar.style.width = "");
+	rpsWindows.forEach(window => {
 		window.classList.remove("win", "loss");
 		window.classList.add("white");
 	});
-	const main = document.querySelector("#main-screen");
 	main.classList.remove("bg-win", "bg-loss");
 	main.classList.add("neutral");
-	bars.forEach(bar => bar.style.width = "");
+	// setTimeout(() => {
+	wrapper.classList.replace("flex", "op-0");
+	wrapper.addEventListener("transitionend", () => {
+		wrapper.classList.replace("op-0", "hide");
+	});
+	// }, 100);
+
 	document.querySelector("#select").firstElementChild
 		.addEventListener("click", roll, { once: true });
-	setTimeout(() => {
-		wrapper.classList.replace("flex", "op-0");
-		wrapper.addEventListener("transitionend", () => {
-			wrapper.classList.replace("op-0", "hide");
-		});
-	}, 500);
-
 }
 
 async function roll() {
